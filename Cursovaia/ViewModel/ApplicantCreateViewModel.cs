abd24e8047bd9a317334fc76f77c630e2fdae573
lapayx <lapayx@mail.ru>
2014-11-26 01:05:51 +0300

    Добавленеи полей

--- Cursovaia/ViewModel/ApplicantCreateViewModel.cs
+++ Cursovaia/ViewModel/ApplicantCreateViewModel.cs
@@ -11,27 +11,52 @@ namespace Cursovaia.ViewModel
     class ApplicantCreateViewModel : BaseViewModel
     {
         readonly IGenericRepository<Applicant> applicant;
-        int? _initializeCommands ;
-        public int? test { get { return this._initializeCommands; }
-            set { this._initializeCommands = value; RaisePropertyChanged("test"); }
+        private Applicant newApplicant;
+       
+        public string firstName { get { return this.newApplicant.FirstName; }
+            set { this.newApplicant.FirstName = value.Trim(); RaisePropertyChanged("firstName"); }
+        }
+        public string secondName
+        {
+            get { return this.newApplicant.SecondName; }
+            set { this.newApplicant.SecondName = value.Trim(); RaisePropertyChanged("secondName"); }
+        }
+        public string fatherName
+        {
+            get { return this.newApplicant.FatherName; }
+            set { this.newApplicant.FatherName = value.Trim(); RaisePropertyChanged("fatherName"); }
         }
         public List<Applicant> sourse { get {
             return this.applicant.SelectAll().ToList();
         } }
         public ApplicantCreateViewModel(IGenericRepository<Applicant> app) {
             this.applicant = app;
+            this.newApplicant = new Applicant();
         }
         
         public override  string this[string columnName]
         {
             get
             {
-                if (columnName == "test" && this._initializeCommands<10)
+                switch (columnName)
                 {
-                    return "Number is not greater than 10!";
+
+                    case "firstName":
+                        if (this.newApplicant.FirstName != null && this.newApplicant.FirstName.Length < 2)
+                            return "Слишком короткое имя";
+                        break;
+                    case "secondName":
+                        if (this.newApplicant.SecondName != null && this.newApplicant.SecondName.Length < 2)
+                            return "Слишком короткая Фамилия";
+                        break;
+                    case "fatherName":
+                        if (this.newApplicant.FatherName != null && this.newApplicant.FatherName.Length < 2)
+                            return "Слишком короткая Отчество";
+                        break;
+                    default :
+                        break;
                 }
-
-
+           
                 return null;
             }
         }
